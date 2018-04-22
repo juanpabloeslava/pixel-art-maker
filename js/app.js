7bfd45e55dd9bc86b73cce2897ca8d04f29d6031
Juan Pablo Eslava <jpesmar@gmail.com>
2018-04-22 13:53:11 +0200

    Added creation of new rows and columns to makeGrid()

--- js/app.js
+++ js/app.js
@@ -14,7 +14,22 @@ function makeGrid() {
 	    table.removeChild(table.lastChild);
 	}
 	// test correct removal of children
-	let noChildren = (table.children.length === 0) ? 'There are no Children on the table' : 'There are ' + table.children.length + ' Children';
-	console.log(noChildren);
+	let noChildren1 = (table.children.length === 0) ? 'There are no Children1 on the table' : 'There are ' + table.children.length + ' Children1';
+	console.log(noChildren1);
+
+	// create new rows and columns in table
+	for (let row=0; row < gridH ; row++) {
+		const tr = document.createElement("tr");
+        table.append(tr);
+        // create columns 
+        for(let col=0 ; col < gridW ; col++){
+            const td = document.createElement("td");
+            tr.append(td);
+        }
+    }
+
+    // test correct removal of children 2
+	let noChildren2 = (table.children.length === 0) ? 'There are no Children2 on the table' : 'There are ' + table.children.length + ' Children2';
+	console.log(noChildren2);
 }
 
